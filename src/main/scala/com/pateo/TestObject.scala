f50554c3349c461c34745f464f0fd88dfb12f61a
chunfengyin <chunfengyin@pateo.com.cn>
2017-08-11 15:22:32 +0800

    init

---
+++ src/main/scala/com/pateo/TestObject.scala
@@ -0,0 +1,12 @@
+package com.pateo
+
+/**
+  * Created by sh04595 on 2017/8/11.
+  */
+object TestObject {
+
+  def main(args: Array[String]): Unit = {
+    print("========= scala idea project ===============")
+
+  }
+}
